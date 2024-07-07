cad9f9387507a91123178a1f2fa62ed2e7b3120b
Crostons <shubh1608003@gmail.com>
2024-07-07 15:31:04 +0530

    Navbar Changes

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -13,7 +13,7 @@ function HeroSection() {
         backgroundPosition: 'center'
       }}
     >
-      <section className="py-12">  {/* Changed background color to dark */}
+      <section className="py-20">  {/* Changed background color to dark */}
       <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="mr-auto place-self-center lg:col-span-7">
           <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-white"><span className='text-emerald-600'>noteFi,</span>Truly Decentralized Options on Canto</h1>  {/* Changed text color to white */}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,31 @@
-"use client";
-import React, { useState } from "react";
+"use client"
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
-import logo from "@/public/noteFi_logo.png";
 import Link from "next/link";
+import logo from "@/public/noteFi_logo.png"; // Ensure this path is correct
 
-export default function Navbar({ className }: { className?: string }) {
-  const [dropdownOpen, setDropdownOpen] = useState(false); // Dropdown state
+function useHideOnScroll() {
+  const [show, setShow] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (typeof window !== "undefined") {
+        setShow(window.scrollY < lastScrollY || window.scrollY < 100);
+        setLastScrollY(window.scrollY);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [lastScrollY]);
+
+  return show;
+}
+
+export default function Navbar() {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const showNavbar = useHideOnScroll();
 
   const toggleDropdown = () => {
     setDropdownOpen(!dropdownOpen);
@@ -16,83 +36,76 @@ export default function Navbar({ className }: { className?: string }) {
   };
 
   return (
-    <header>
-      <nav className="bg-gray-900 border-gray-700 px-4 lg:px-6 py-2.5 dark:bg-gray-900 border-b">
-        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-          <div className="flex items-center">
-            <Image className="h-10 w-10 mr-2" src={logo} alt="noteFi" />
-            <span className="self-center text-xl font-semibold whitespace-nowrap text-white">noteFi</span>
-          </div>
-          <div className="flex items-center lg:order-2">
-            <div className="px-4 lg:px-5 py-2 lg:py-2.5 mr-2">
-              <w3m-button />
-            </div>
-            <button
-              data-collapse-toggle="mobile-menu-2"
-              type="button"
-              className="inline-flex items-center p-2 ml-1 text-sm text-gray-400 rounded-lg lg:hidden hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-600"
-              aria-controls="mobile-menu-2"
-              aria-expanded="false"
-            >
-              <span className="sr-only">Open main menu</span>
-              <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  fillRule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </button>
-          </div>
-          <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
-            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-              <Link href="/">
-                <li className="block py-2 pr-4 pl-3 text-emerald-400 rounded bg-transparent lg:bg-transparent lg:text-emerald-400 lg:p-0 dark:text-white" aria-current="page" onClick={closeDropdown}>
-                  Home
-                </li>
-              </Link>
-              <li className="relative">
-                <div
-                  className={`flex items-center cursor-pointer block py-2 pr-4 pl-3 text-gray-400 border-b border-gray-700 hover:bg-gray-800 lg:hover:bg-transparent lg:border-0 lg:hover:text-emerald-400 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-800 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700 ${dropdownOpen ? 'text-emerald-400' : ''}`}
-                  onClick={toggleDropdown}
-                >
-                  Options
-                  <svg
-                    className={`w-4 h-4 ml-1 text-gray-400 transform transition-transform ${dropdownOpen ? 'rotate-180' : ''}`}
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7"></path>
-                  </svg>
-                </div>
-                {dropdownOpen && (
-                  <div className="mt-8 absolute z-10 bg-gray-800 rounded-lg shadow w-44 dark:bg-gray-800">
-                    <ul className="py-2 text-sm text-gray-200 dark:text-gray-200">
-                      <Link href="/write">
-                        <li className="block px-4 py-2 hover:bg-gray-700 dark:hover:bg-gray-700 dark:hover:text-white" onClick={closeDropdown}>
-                          Write Option
-                        </li>
-                      </Link>
-                      <Link href="/buy">
-                        <li className="block px-4 py-2 hover:bg-gray-700 dark:hover:bg-gray-700 dark:hover:text-white" onClick={closeDropdown}>
-                          Buy Option
-                        </li>
-                      </Link>
-                    </ul>
-                  </div>
-                )}
-              </li>
-              <Link href="/positions">
-                <li className="block py-2 pr-4 pl-3 text-gray-400 border-b border-gray-700 hover:bg-gray-800 lg:hover:bg-transparent lg:border-0 lg:hover:text-emerald-400 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-800 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700" onClick={closeDropdown}>
-                  Positions
-                </li>
-              </Link>
-            </ul>
-          </div>
+    <nav className={`fixed w-full z-20 top-0 bg-gray-900 border-b border-gray-600 transition-transform duration-300 ${showNavbar ? 'translate-y-0' : '-translate-y-full'}`}>
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <div className="flex items-center space-x-3 rtl:space-x-reverse">
+          <Image src={logo} className="w-10 h-10" alt="noteFi Logo" />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">noteFi</span>
         </div>
-      </nav>
-    </header>
+        <div className="flex items-center lg:order-2">
+          <div className="px-4">
+            <w3m-button label="Connect Wallet" /> {/* Assuming w3m-button is a component you have */}
+          </div>
+          <button
+            data-collapse-toggle="navbar-hamburger"
+            type="button"
+            className="inline-flex items-center p-2 text-gray-400 rounded-lg lg:hidden hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-600"
+            aria-controls="navbar-hamburger"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+          </button>
+        </div>
+        <div className="hidden items-center justify-end w-full lg:flex lg:w-auto lg:order-1" id="navbar-sticky">
+          <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+            <Link href="/">
+              <li className="block py-2 pr-4 pl-3 text-white hover:bg-gray-700 lg:hover:bg-transparent lg:p-0" onClick={closeDropdown}>
+                Home
+              </li>
+            </Link>
+            <li className="relative">
+              <div
+                className={`flex items-center cursor-pointer py-2 pr-4 pl-3 text-white hover:bg-gray-700 lg:hover:bg-transparent lg:p-0 ${dropdownOpen ? 'text-white' : ''}`}
+                onClick={toggleDropdown}
+              >
+                Options
+                <svg
+                  className={`w-4 h-4 ml-1 transform transition-transform ${dropdownOpen ? 'rotate-180' : ''}`}
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path>
+                </svg>
+              </div>
+              {dropdownOpen && (
+                <div className="mt-8 absolute z-10 bg-gray-800 rounded-lg shadow w-44">
+                  <ul className="py-2 text-sm text-white">
+                    <Link href="/write">
+                      <li className="block px-4 py-2 hover:bg-gray-700" onClick={closeDropdown}>
+                        Write Option
+                      </li>
+                    </Link>
+                    <Link href="/buy">
+                      <li className="block px-4 py-2 hover:bg-gray-700" onClick={closeDropdown}>
+                        Buy Option
+                      </li>
+                    </Link>
+                  </ul>
+                </div>
+              )}
+            </li>
+            <Link href="/positions">
+              <li className="block py-2 pr-4 pl-3 text-white hover:bg-gray-700 lg:hover:bg-transparent lg:p-0" onClick={closeDropdown}>
+                Positions
+              </li>
+            </Link>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
